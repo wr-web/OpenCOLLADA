cb1d1c73444d26884634aa1bc0e5410cd52a1371
Wang Rui <55612496+wr-web@users.noreply.github.com>
2026-05-10 22:52:56 +0800

    Fix reference counting for PyList_SetItem
    Remove unnecessary reference count decrement for newName.

--- Externals/LibXML/python/libxml.c
+++ Externals/LibXML/python/libxml.c
@@ -1172,7 +1172,6 @@ pythonAttributeDecl(void *user_data,
         for (node = tree; node != NULL; node = node->next) {
             newName = PyString_FromString((char *) node->name);
             PyList_SetItem(nameList, count, newName);
-	    Py_DECREF(newName);
             count++;
         }
         result = PyObject_CallMethod(handler, (char *) "attributeDecl",
